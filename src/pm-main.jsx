7e11d3d5369f854ada3848a7a41e832dcc365a0c
vicki <jackljl007@hotmail.com>
2026-04-25 12:30:06 +0800

    fix(theme): WS-0 补全 light tokens + PM entry 加载

--- src/pm-main.jsx
+++ src/pm-main.jsx
@@ -3,6 +3,7 @@ applyBootstrapTheme();
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import "./pm-index.css";
 import ProjectManager from "./ProjectManager";
 import { ThemeProvider } from "./contexts/ThemeContext.jsx";
